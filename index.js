8484483390e7be3adc29ef3c548a750407c7d6e5
salemalem <shyngys.shynbolatov@gmail.com>
2023-08-31 18:13:49 +0700

    redis middleware is cracked

--- index.js
+++ index.js
@@ -3,22 +3,65 @@ const fs = require('fs');
 const app = express();
 const PORT = 3000;
 
+const redis = require('redis');
+const client = redis.createClient();
+
+client.on('connect', function() {
+  console.log('Redis client is connected!'); // Connected!
+});
+client.on('ready', () => {
+  console.log('Redis client is ready');
+});
+client.on('error', (err) => {
+  console.error('Redis error:', err);
+});
+
 // Middleware to read JSON files
 const readJSONFile = (filename) => {
     return JSON.parse(fs.readFileSync(filename, 'utf8'));
 };
 
-app.get('/v1/structure', (req, res) => {
+const cacheMiddleware = (topic, idKeyExtractor) => {
+  try {
+    return (req, res, next) => {
+        const id = idKeyExtractor(req);
+        const cacheKey = `${topic}:${id}`;
+
+        client.get(cacheKey, (err, data) => {
+            if (err) throw err;
+
+            if (data !== null) {
+                res.send(JSON.parse(data));
+            } else {
+                next();
+            }
+        });
+    }; 
+  } catch (error) {
+    console.error('Cached Middleware Error:', error);
+    res.status(500).send('Internal Server Error');
+  }
+};
+
+
+app.get('/v1/structure', async (req, res) => {
+  try {
     const { projectId, locationType } = req.query;
     if (locationType === 'building') {
         const buildings = readJSONFile('./buildings.json').buildings;
         const building = buildings.find(b => b.id === projectId);
         if (building) {
-            return res.json(building);
+          const cacheKey = `Buildings:${req.query.projectId}`;
+          client.setEx(cacheKey, 3600, JSON.stringify(building)); // TTL set to 1 hour (3600 seconds) as an example
+          return res.json(building);
         }
         return res.status(404).json({ error: 'Building not found' });
     }
     res.status(400).json({ error: 'Invalid location type' });
+  } catch (error) {
+    console.error('Structure Endpoint Error:', error);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 app.get('/v2/Checklists', (req, res) => {
